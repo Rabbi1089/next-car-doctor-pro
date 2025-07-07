c22599f4e66042d223266a43dc89ee3a9a73b55f
Rabbi1089 <rabbi.ext@gmail.com>
2025-07-07 23:29:33 +0600

    Add service details

--- src/app/services/[id]/page.jsx
+++ src/app/services/[id]/page.jsx
@@ -1,14 +1,11 @@
-import dbConnect, { collectionNameObj } from "@/lib/dbConnect";
-import { ObjectId } from "mongodb";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 async function page({ params }) {
   const p = await params;
-  const serviceCollection = dbConnect(collectionNameObj.serviceCollection);
-  const data = await serviceCollection.findOne({
-    _id: new ObjectId(p.id),
-  });
+  const res = await fetch(`http://localhost:3000/api/service/${p.id}`);
+  const data = await res.json();
   return (
     <>
       <div className=" w-[1200px] mx-auto">
@@ -33,10 +30,14 @@ async function page({ params }) {
           <div className="md:w-3/5 gap-2 mx-auto">
             <Image src={data.img} width={600} height={200} alt={data.title} />
           </div>
-          <div className="md:w-1/5   mx-auto ">
-            <button className=" btn btn-ghost bg-primary text-white uppercase w-full">
+          <div className="md:w-1/5 mx-auto ">
+            <Link
+              className=" btn btn-ghost bg-primary text-white uppercase w-full"
+              href={`/checkout/${data._id}`}
+            >
               checkout
-            </button>
+            </Link>
+
             <h1 className="text-center mt-4 text-2xl font-semibold text-black">
               Price: $ <span className=" text-primary">{data.price}</span>
             </h1>
